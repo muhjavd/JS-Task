95b0206f6e8e402708aae67d9ef870fd8a1f1284
muhammad javed <javedkhan3989@gmail.com>
2025-11-25 22:58:07 +0500

    JS task Update

--- time.js
+++ time.js
@@ -1,5 +1,14 @@
-function convertHours() {
-  const hours = document.getElementById("hours").value;
-  const seconds = hours * 3600;
-  document.getElementById("secondsResult").innerText = `${seconds} seconds`;
-}
+ function convertHours() {
+      const hoursInput = document.getElementById("hours");
+      const hours = Number(hoursInput.value);
+      const result = document.getElementById("secondsResult");
+
+      if (hours <= 0 || isNaN(hours)) {
+        hoursInput.value ="Invalid"; 
+        result.innerText = ""; 
+        return; 
+      }
+
+      const seconds = hours * 3600;
+      result.innerText = `${seconds} seconds`;
+    }
